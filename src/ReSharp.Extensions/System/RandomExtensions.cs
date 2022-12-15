53380a58ca6cbbfb3e5a1e60412978c6e58ee9e6
Jerry Lee <cosmos53076@163.com>
2022-12-15 17:13:12 +0800

    Change classes RandomUtility and RandomExtensions.

--- src/ReSharp.Extensions/System/RandomExtensions.cs
+++ src/ReSharp.Extensions/System/RandomExtensions.cs
@@ -11,48 +11,22 @@ namespace ReSharp.Extensions
     public static class RandomExtensions
     {
         /// <summary>
-        /// Returns a random double that is within a specific range.
+        /// Returns a non-negative random integer.
         /// </summary>
-        /// <param name="source">The <see cref="Random"/> to return a random <see cref="double"/> value.</param>
-        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
-        /// <param name="maxValue">
-        /// The exclusive upper bound of the random number returned. <c>maxValue</c> must be greater
-        /// than or equal to <c>minValue</c>.
-        /// </param>
+        /// <param name="source">The <see cref="Random"/> to return a random 64-bit signed integer.</param>
+        /// <returns>A 64-bit signed integer that is greater than or equal to 0 and less than <see cref="Int64.MaxValue"/>.</returns>
+        public static long RangeInt64(this Random source) => source.RangeInt64(0, long.MaxValue);
+        
+        /// <summary>
+        /// Returns a non-negative random integer that is less than the specified maximum.
+        /// </summary>
+        /// <param name="source">The <see cref="Random"/> to return a random 64-bit signed integer.</param>
+        /// <param name="maxValue">The exclusive upper bound of the random number to be generated. maxValue must be greater than or equal to 0.</param>
         /// <returns>
-        /// A double greater than or equal to <c>minValue</c> and less than <c>maxValue</c>; that
-        /// is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>. If
-        /// <c>minValue</c> equals <c>maxValue</c>, <c>minValue</c> is returned.
+        /// A 64-bit signed integer that is greater than or equal to 0, and less than <c>maxValue</c>;
+        /// that is, the range of return values ordinarily includes 0 but not <c>maxValue</c>. However, if <c>maxValue</c> equals 0, <c>maxValue</c> is returned.
         /// </returns>
-        /// <exception cref="ArgumentOutOfRangeException"><c>minValue</c> is greater than <c>maxValue</c>.</exception>
-        public static double NextDouble(this Random source, double minValue, double maxValue)
-        {
-            if (minValue > maxValue)
-                throw new ArgumentOutOfRangeException(nameof(minValue), "minValue is greater than maxValue.");
-
-            var range = maxValue - minValue;
-            double result;
-
-            if (range <= double.MaxValue)
-            {
-                result = source.NextDouble() * range + minValue;
-            }
-            else
-            {
-                result = source.NextDouble() * double.MaxValue + minValue;
-            }
-
-            if (double.IsPositiveInfinity(result))
-            {
-                result = double.MaxValue;
-            }
-            else if (double.IsNegativeInfinity(result))
-            {
-                result = double.MinValue;
-            }
-
-            return result;
-        }
+        public static long RangeInt64(this Random source, long maxValue) => source.RangeInt64(0, maxValue);
 
         /// <summary>
         /// Returns a random 64-bit signed integer that is within a specific range.
@@ -69,7 +43,7 @@ namespace ReSharp.Extensions
         /// <c>maxValue</c>. If <c>minValue</c> equals <c>maxValue</c>, <c>minValue</c> is returned.
         /// </returns>
         /// <exception cref="ArgumentOutOfRangeException"><c>minValue</c> is greater than <c>maxValue</c>.</exception>
-        public static long NextLong(this Random source, long minValue, long maxValue)
+        public static long RangeInt64(this Random source, long minValue, long maxValue)
         {
             if (minValue > maxValue)
                 throw new ArgumentOutOfRangeException(nameof(minValue), "minValue is greater than maxValue.");
@@ -90,6 +64,13 @@ namespace ReSharp.Extensions
         }
 
         /// <summary>
+        /// Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0.
+        /// </summary>
+        /// <param name="source">The <see cref="Random"/> to return a random floating-point number.</param>
+        /// <returns>A single-precision floating point number that is greater than or equal to 0.0, and less than 1.0.</returns>
+        public static float RangeSingle(this Random source) => source.RangeSingle(0, 1);
+
+        /// <summary>
         /// Returns a random single that is within a specific range.
         /// </summary>
         /// <param name="source">The <see cref="Random"/> to return a single double.</param>
@@ -99,12 +80,12 @@ namespace ReSharp.Extensions
         /// than or equal to <c>minValue</c>.
         /// </param>
         /// <returns>
-        /// A single greater than or equal to <c>minValue</c> and less than <c>maxValue</c>; that
-        /// is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>. If
-        /// <c>minValue</c> equals <c>maxValue</c>, <c>minValue</c> is returned.
+        /// A single greater than or equal to <c>minValue</c> and less than <c>maxValue</c>;
+        /// that is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>.
+        /// If <c>minValue</c> equals <c>maxValue</c>, <c>minValue</c> is returned.
         /// </returns>
         /// <exception cref="ArgumentOutOfRangeException"><c>minValue</c> is greater than <c>maxValue</c>.</exception>
-        public static float NextSingle(this Random source, float minValue, float maxValue)
+        public static float RangeSingle(this Random source, float minValue, float maxValue)
         {
             if (minValue > maxValue)
                 throw new ArgumentOutOfRangeException(nameof(minValue), "minValue is greater than maxValue.");
@@ -128,6 +109,50 @@ namespace ReSharp.Extensions
             else if (float.IsNegativeInfinity(result))
             {
                 result = float.MinValue;
+            }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Returns a random double that is within a specific range.
+        /// </summary>
+        /// <param name="source">The <see cref="Random"/> to return a random <see cref="double"/> value.</param>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">
+        /// The exclusive upper bound of the random number returned. <c>maxValue</c> must be greater
+        /// than or equal to <c>minValue</c>.
+        /// </param>
+        /// <returns>
+        /// A double greater than or equal to <c>minValue</c> and less than <c>maxValue</c>; that
+        /// is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>. If
+        /// <c>minValue</c> equals <c>maxValue</c>, <c>minValue</c> is returned.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"><c>minValue</c> is greater than <c>maxValue</c>.</exception>
+        public static double RangeDouble(this Random source, double minValue, double maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException(nameof(minValue), "minValue is greater than maxValue.");
+
+            var range = maxValue - minValue;
+            double result;
+
+            if (range <= double.MaxValue)
+            {
+                result = source.NextDouble() * range + minValue;
+            }
+            else
+            {
+                result = source.NextDouble() * double.MaxValue + minValue;
+            }
+
+            if (double.IsPositiveInfinity(result))
+            {
+                result = double.MaxValue;
+            }
+            else if (double.IsNegativeInfinity(result))
+            {
+                result = double.MinValue;
             }
 
             return result;
--- src/ReSharp.Extensions/System/RandomUtility.cs
+++ src/ReSharp.Extensions/System/RandomUtility.cs
@@ -11,22 +11,104 @@ namespace ReSharp.Extensions
     public static class RandomUtility
     {
         /// <summary>
-        /// Returns a random floating-point number that is greater than or equal to <b>minValue</b>, and less than <b>maxValue</b>.
+        /// Gets the <see cref="Random"/> object with random seed initialized.
         /// </summary>
-        /// <param name="minValue">The inclusive lower bound of the random floating-point number returned. </param>
-        /// <param name="maxValue">The exclusive upper bound of the random floating-point number returned.
-        /// <b>maxValue</b> must be greater than or equal to <b>minValue</b>. </param>
-        /// <returns>A floating-point number is greater than or equal to <b>minValue</b>, and less than <b>maxValue</b>. </returns>
-        /// <exception cref="ArgumentOutOfRangeException"><b>minValue</b> is greater than <b>maxValue</b>. </exception>
-        public static double NextDouble(double minValue, double maxValue)
-        {
-            if (minValue > maxValue)
-                throw new ArgumentOutOfRangeException(nameof(minValue));
-            
-            var random = new Random(Guid.NewGuid().GetHashCode());
-            return random.NextDouble() * (maxValue - minValue) + minValue;
-        }
+        public static Random RandomWithSeed => new Random(Guid.NewGuid().GetHashCode());
         
+        /// <summary>
+        /// Fills the elements of a specified array of bytes with random numbers on random seed initialized.
+        /// </summary>
+        /// <param name="buffer">The array to be filled with random numbers.</param>
+        public static void NextBytesWithRandomSeed(byte[] buffer) => RandomWithSeed.NextBytes(buffer);
+        
+        /// <summary>
+        /// Returns a non-negative random integer on random seed initialized.
+        /// </summary>
+        /// <returns>A 32-bit signed integer that is greater than or equal to 0 and less than <see cref="Int32.MaxValue"/>.</returns>
+        public static int NextWithRandomSeed() => RandomWithSeed.Next();
+        
+        /// <summary>
+        /// Returns a non-negative random integer that is less than the specified maximum on random seed initialized.
+        /// </summary>
+        /// <param name="maxValue">The exclusive upper bound of the random number to be generated. <c>maxValue</c> must be greater than or equal to 0.</param>
+        /// <returns>
+        ///A 32-bit signed integer that is greater than or equal to 0, and less than <c>maxValue</c>;
+        /// that is, the range of return values ordinarily includes 0 but not <c>maxValue</c>. However, if <c>maxValue</c> equals 0, <c>maxValue</c> is returned.
+        /// </returns>
+        public static int NextWithRandomSeed(int maxValue) => RandomWithSeed.Next(maxValue);
+        
+        /// <summary>
+        /// Returns a random integer that is within a specified range on random seed initialized.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive upper bound of the random number returned. <c>maxValue</c> must be greater than or equal to <c>minValue</c>.</param>
+        /// <returns>
+        /// A 32-bit signed integer greater than or equal to <c>minValue</c> and less than <c>maxValue</c>;
+        /// that is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>. If minValue equals <c>maxValue</c>, minValue is returned.
+        /// </returns>
+        public static int NextWithRandomSeed(int minValue, int maxValue) => RandomWithSeed.Next(minValue, maxValue);
+
+        /// <summary>
+        /// Returns a non-negative random integer on random seed initialized.
+        /// </summary>
+        /// <returns>A 64-bit signed integer that is greater than or equal to 0 and less than <see cref="Int64.MaxValue"/>.</returns>
+        public static long NextInt64WithRandomSeed() => RandomWithSeed.RangeInt64();
+        
+        /// <summary>
+        /// Returns a non-negative random integer that is less than the specified maximum on random seed initialized.
+        /// </summary>
+        /// <param name="maxValue">The exclusive upper bound of the random number to be generated. <c>maxValue</c> must be greater than or equal to 0.</param>
+        /// <returns>
+        ///A 64-bit signed integer that is greater than or equal to 0, and less than <c>maxValue</c>;
+        /// that is, the range of return values ordinarily includes 0 but not <c>maxValue</c>. However, if <c>maxValue</c> equals 0, <c>maxValue</c> is returned.
+        /// </returns>
+        public static long NextInt64WithRandomSeed(long maxValue) => RandomWithSeed.RangeInt64(maxValue);
+        
+        /// <summary>
+        /// Returns a random integer that is within a specified range on random seed initialized.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive upper bound of the random number returned. <c>maxValue</c> must be greater than or equal to <c>minValue</c>.</param>
+        /// <returns>
+        /// A 64-bit signed integer greater than or equal to <c>minValue</c> and less than <c>maxValue</c>;
+        /// that is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>. If minValue equals <c>maxValue</c>, minValue is returned.
+        /// </returns>
+        public static long NextInt64WithRandomSeed(long minValue, long maxValue) => RandomWithSeed.RangeInt64(minValue, maxValue);
+
+        /// <summary>
+        /// Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0 on random seed initialized.
+        /// </summary>
+        /// <returns>A single-precision floating point number that is greater than or equal to 0.0, and less than 1.0.</returns>
+        public static float NextSingleWithRandomSeed() => RandomWithSeed.RangeSingle();
+        
+        /// <summary>
+        /// Returns a random floating-point number that is within a specified range on random seed initialized.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive upper bound of the random number returned. <c>maxValue</c> must be greater than or equal to <c>minValue</c>.</param>
+        /// <returns>
+        /// A single-precision floating point number greater than or equal to <c>minValue</c> and less than <c>maxValue</c>;
+        /// that is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>. If minValue equals <c>maxValue</c>, minValue is returned.
+        /// </returns>
+        public static float NextSingleWithRandomSeed(float minValue, float maxValue) => RandomWithSeed.RangeSingle(minValue, maxValue);
+        
+        /// <summary>
+        /// Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0 on random seed initialized.
+        /// </summary>
+        /// <returns>A double-precision floating point number that is greater than or equal to 0.0, and less than 1.0.</returns>
+        public static double NextDoubleWithRandomSeed() => RandomWithSeed.RangeDouble(0, 1);
+        
+        /// <summary>
+        /// Returns a random floating-point number that is within a specified range on random seed initialized.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive upper bound of the random number returned. <c>maxValue</c> must be greater than or equal to <c>minValue</c>.</param>
+        /// <returns>
+        /// A double-precision floating point number greater than or equal to <c>minValue</c> and less than <c>maxValue</c>;
+        /// that is, the range of return values includes <c>minValue</c> but not <c>maxValue</c>. If minValue equals <c>maxValue</c>, minValue is returned.
+        /// </returns>
+        public static double NextDoubleWithRandomSeed(double minValue, double maxValue) => RandomWithSeed.RangeDouble(minValue, maxValue);
+
         /// <summary>
         /// Returns a sequence with random floating-point numbers that are greater than or equal to <b>minValue</b>, and less than <b>maxValue</b>.
         /// </summary>
@@ -50,7 +132,7 @@ namespace ReSharp.Extensions
             while (i < count)
             {
                 var endValue = Math.Min(startValue + unitValue, maxValue);
-                sequence[i] = NextDouble(startValue, endValue);
+                sequence[i] = NextDoubleWithRandomSeed(startValue, endValue);
                 startValue = endValue;
                 i++;
             }
